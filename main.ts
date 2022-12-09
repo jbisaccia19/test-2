b4e17eb4e1f5b7c3c374efafd0430aa310322071
jbisaccia19 <119688482+jbisaccia19@users.noreply.github.com>
2022-12-09 12:47:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 info.onScore(100, function () {
 	
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
 let input2 = game.askForString("What's your name?")
 game.splash("Hello " + input2 + "!")
 if (true) {
